f997e729eff7a5821e0f99285e866a094d4da40e
aviinfo <96535646+aviinfo@users.noreply.github.com>
2022-01-11 19:01:46 +0200

    Create laba.c

---
+++ laba.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+float r, h;
+float obiemcilindra;
+printf("vvedite radius cilindra\n");
+scanf("%f", &r);
+printf("vvedite visotu cilindra\n");
+scanf("%f", &h);
+obiemcilindra=3.14*r*r*h;
+printf("obiem cilindra = %f", obiemcilindra);
+printf("\n\n\n\n");
+}
